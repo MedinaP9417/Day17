97557dc3ed2b12702b43232cbcb506668e6411be
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-25 08:02:34 +0100

    Added Assignment for Day17

---
+++ ex08/src/index.js
@@ -0,0 +1,38 @@
+// Lodash
+const _ = require('lodas');
+
+// Only change code below this line
+// users nested array with four objects starts here
+const user = [{
+            "firstName": "John",
+            "last<name": "Doe,
+            "age": 24,
+            "gender": "male"
+        },
+        {
+
+        }
+    ]
+    // users nested array with four objects ends here 
+    // getUsers function - list of users starts here
+
+
+// getUsers function - list od users ends here
+
+// findUser(lastName, gender) function starts here
+function findUser(lastName, gender) {
+    try {
+        // add appropriate code here
+    )
+    catch (error) {
+        return // Change this line
+        console.log(); // Change this line
+    }
+}
+// findUser(lastName, gender) function ends here
+// Only change code above this line
+getUsers();
+findUser(); // Change this line
+
+module.exports = findUser;
+}
